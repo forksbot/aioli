6f22081b6aa54421777a54c04035eeea63ae9ade
Robert Wikman <rbw@vault13.org>
2019-04-14 20:30:05 +0200

    Add more exceptions

--- aioli/exceptions.py
+++ aioli/exceptions.py
@@ -9,7 +9,16 @@ class InternalError(HTTPException):
 
 
 class AioliException(HTTPException):
-    def __init__(self, *args, **kwargs):
-        self.log_message = kwargs.pop('write_log', False)
+    def __init__(self, status=500, message='Internal Server Error'):
+        super(AioliException, self).__init__(status_code=status, detail=message)
 
-        super(AioliException, self).__init__(*args, **kwargs)
+
+class DatabaseError(HTTPException):
+    def __init__(self):
+        super(DatabaseError, self).__init__(status_code=500, detail='Database error')
+
+
+class NoMatchFound(HTTPException):
+    def __init__(self, message='Not Found'):
+        super(NoMatchFound, self).__init__(status_code=404, detail=message)
+
