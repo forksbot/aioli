d56e40bbb1fc108149e7d546cf0bfd68b2f38caf
Robert Wikman <rbw@vault13.org>
2019-05-30 16:18:33 +0200

    Rework request handler decorators

--- aioli/package/controller/decorators.py
+++ aioli/package/controller/decorators.py
@@ -11,64 +11,72 @@ from starlette.requests import Request
 from starlette.responses import Response
 
 from aioli.registry import RouteRegistry
-from aioli.consts import Method
+from aioli.exceptions import AioliException
+
+from .consts import Method, RequestProp
 
 
-def output_dump(schema, status=200, many=False):
-    """Returns a transformed and serialized Response
+def route(path, method, description=None):
+    """Prepares route registration, and performs handler injection.
 
-    :param schema: Marshmallow.Schema class
-    :param status: Return status (on success)
-    :param many: Whether to return a list or single object
-    :return: Response
+    :param path: Handler path, relative to application and package paths
+    :param method: HTTP Method
+    :param description: Endpoint description
+    :return: Route handler
     """
-
-    schema = schema(many=many)
 
     def wrapper(fn):
         @wraps(fn)
         async def handler(*args, **kwargs):
-            args_new = list(args)
+            return await fn(*args, **kwargs)
 
-            # Remove `Request` object from args (in case it wasn't consumed by `input_local`).
-            if len(args) > 1 and isinstance(args[1], Request):
-                args_new.pop(1)
-
-            rv = await fn(*args_new, **kwargs)
-
-            # Return HTTP encoded JSON response
-            return Response(
-                content=schema.dumps(rv, indent=4, ensure_ascii=False).encode('utf8'),
-                status_code=status,
-                headers={'content-type': 'application/json'}
+        if not isinstance(method, Method):
+            raise AioliException(
+                f"Invalid HTTP method supplied in @route for handler: {handler}. "
+                f"Must be of type: {Method.__module__}.{Method.__name__}"
             )
 
         stack = RouteRegistry.get_stack(handler)
 
-        # Add the `response` schema to this route handler's stack
-        stack.schemas.update({'response': schema})
+        # Adds the handler for registration once the loop is ready.
+        stack.add_route(handler, path, method.value, description)
 
         return handler
 
     return wrapper
 
 
-def input_load(**schemas):
+def takes(props=None, **schemas):
     """Takes a list of schemas used to validate and transform parts of a request object.
     The selected parts are injected into the route handler as arguments.
 
+    :param props: List of `Pluck` targets
     :param schemas: list of schemas (kwargs)
     :return: Route handler
     """
 
+    props = props or []
     schemas = {part: schema() for part, schema in schemas.items()}
 
     def wrapper(fn):
         @wraps(fn)
         async def handler(*args, **kwargs):
             args_new = list(args)
-
             request = kwargs['request'] if 'request' in kwargs else args_new.pop(1)
+
+            for prop in props:
+                value = RequestProp(prop).value
+                assert "." in value
+
+                target = request
+
+                for member in value.split('.'):
+                    target = getattr(target, member)
+
+                kwargs.update({RequestProp(prop).name: target})
+
+            if 'headers' in schemas:
+                kwargs.update({'headers': schemas['headers'].load(request.headers)})
 
             if 'path' in schemas:
                 kwargs.update(schemas['path'].load(request.path_params))
@@ -92,46 +100,40 @@ def input_load(**schemas):
     return wrapper
 
 
-def route(path, method, description=None, inject=None):
-    """Prepares route registration, and performs handler injection.
+def returns(schema_cls=None, status=200, many=False):
+    """Returns a transformed and serialized Response
 
-    :param path: Handler path, relative to application and package paths
-    :param method: HTTP Method
-    :param inject: List of `Injector` injections
-    :param description: Endpoint description
-    :return: Route handler
+    :param schema_cls: Marshmallow.Schema class
+    :param status: Return status (on success)
+    :param many: Whether to return a list or single object
+    :return: Response
     """
 
-    injections = inject or []
-
-    if isinstance(method, str):
-        method = Method(method.upper()).value
+    schema = schema_cls(many=many) if schema_cls else None
 
     def wrapper(fn):
         @wraps(fn)
-        async def handler(*inner_args, **inner_kwargs):
-            request = inner_args[1]
-            args_new = list(inner_args)
+        async def handler(*args, **kwargs):
+            args_new = list(args)
 
-            # Handle injections and inject as kwargs.
-            for injection in injections:
-                if callable(injection.value):
-                    func = injection.value
-                    value = func(request)
-                elif injection.name == 'request':
-                    # Full request injection, remove duplicate.
-                    value = args_new.pop(1)
-                else:
-                    raise Exception('Unknown injection')
+            # Remove `Request` object from args (in case it wasn't consumed by an `input_local`).
+            if len(args) > 1 and isinstance(args[1], Request):
+                args_new.pop(1)
 
-                inner_kwargs.update({injection.name: value})
+            rv = await fn(*args_new, **kwargs)
+            data = schema.dumps(rv, indent=4, ensure_ascii=False).encode('utf8') if schema else None
 
-            return await fn(*args_new, **inner_kwargs)
+            # Return HTTP encoded JSON response
+            return Response(
+                content=data,
+                status_code=status,
+                headers={'content-type': 'application/json'}
+            )
 
         stack = RouteRegistry.get_stack(handler)
 
-        # Adds the handler for registration once the loop is ready.
-        stack.add_route(handler, path, method, description)
+        # Add the `response` schema to this route handler's stack
+        stack.schemas.update({'response': schema})
 
         return handler
 
