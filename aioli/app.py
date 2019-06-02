23af83045a836521173cbd70d1dbd3dbf637f336
Robert Wikman <rbw@vault13.org>
2019-06-02 22:40:30 +0200

    Rework the app module

--- aioli/app.py
+++ aioli/app.py
@@ -12,13 +12,17 @@ from marshmallow.exceptions import ValidationError
 from aioli.exceptions import HTTPException, AioliException
 from aioli.log import LOGGING_CONFIG_DEFAULTS
 from aioli.utils.http import jsonify
-from .settings import ApplicationSettings
-from .manager import Manager
+from .config import ConfigConsumer
+from .manager import PackageManager
 
 
 async def server_error(_, exc):
     if isinstance(exc, NotImplementedError):
-        return jsonify({"message": "Not implemented"}, status=500)
+        message = "Not implemented"
+    else:
+        message = "Internal server error"
+
+    return jsonify({"message": message}, status=500)
 
 
 async def validation_error(_, exc):
@@ -34,62 +38,60 @@ async def http_error(_, exc):
 
 
 class Application(Starlette):
-    """Creates a Aioli application
+    """Creates an Aioli application
 
-    :param packages: List of (<path>, <package>)
-    :param path: Application root path
-    :param settings: Settings overrides
-    :param kwargs: kwargs to pass along to Sanic
+    :param packages: List of package tuples [("<path>", <pkg>), ...]
+    :param listen_host: Bind socket to this host
+    :param listen_port: Bind socket to this port
+    :param debug: Debug mode
+    :param workers: Worker count
+    :param api_base: API base path
+    :param db_url: SQLAlchemy database URL
+    :param kwargs: Kwargs to pass along to Starlette
     """
+
+    log = logging.getLogger("aioli")
 
     async def startup(self):
         try:
-            await self.mgr.attach(self)
+            await self.pkg_mgr.attach(self)
             self.log.info("Ready for action")
         except Exception as e:
             self.log.critical(traceback.format_exc())
             raise e
 
     async def shutdown(self):
-        self.log.info("Disconnecting from database...")
-        await self.mgr.db.database.disconnect()
+        self.pkg_mgr.log.info("Disconnecting from database...")
+        await self.pkg_mgr.db.database.disconnect()
 
-    def __init__(
-        self, packages=None, path="/api", cors_options=None, settings=None, **kwargs
-    ):
-        # super(Application, self).__init__(False, [])
-
+    def __init__(self, packages, **kwargs):
         if not packages:
             raise Exception(
                 f"aioli.Application expects an iterable of packages, got: {type(packages)}"
             )
 
-        self.cors_options = cors_options or {}
         self.packages = packages
 
         try:
-            overrides = dict(settings or {})
+            overrides = dict(kwargs or {})
         except ValueError:
             raise Exception("Application `settings` must be a collection")
 
-        super(Application, self).__init__(
-            # log_config=kwargs.pop('log_config', LOGGING_CONFIG_DEFAULTS),
-            **kwargs
-        )
-
-        self.mgr = Manager()
+        self.pkg_mgr = PackageManager()
 
         for name, logger in LOGGING_CONFIG_DEFAULTS['loggers'].items():
-            logger['level'] = 'DEBUG' if overrides.get('DEBUG') else 'INFO'
+            self.log_level = logger['level'] = 'DEBUG' if overrides.get('debug') else 'INFO'
 
         logging.config.dictConfig(LOGGING_CONFIG_DEFAULTS)
 
-        # Application root logger
-        self.log = logging.getLogger("aioli.core")
         self.log.info("Commencing countdown, engines on")
 
         # Apply known settings from ENV or provided `settings`
-        self.config = ApplicationSettings(overrides, path).merged
+        cc = ConfigConsumer(overrides)
+        self.config = cc.consumed
+
+        # Pass the remainder arguments to Starlette
+        super(Application, self).__init__(**cc.consumables)
 
         # Lifespan handlers
         self.router.lifespan.add_event_handler("startup", self.startup)
@@ -100,7 +102,7 @@ class Application(Starlette):
         self.add_exception_handler(HTTPException, http_error)
         self.add_exception_handler(ValidationError, validation_error)
         self.add_exception_handler(JSONDecodeError, decode_error)
-        # self.add_exception_handler(Exception, server_error)
+        self.add_exception_handler(Exception, server_error)
 
         # Middleware
         self.add_middleware(CORSMiddleware, allow_origins=["*"])
